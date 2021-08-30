2cd6eb967c984478ab352dc063d435a7f3d07160
Joy Fu <lingyi.fu@duke.edu>
2021-08-30 13:07:48 -0400

    Write HDL analysis and driver

--- blood_calculator.py
+++ blood_calculator.py
@@ -3,12 +3,35 @@ def interface():
     keep_running = True
     while keep_running:
         print("Make a choice")
+        print("1 - HDL Analysis")
         print("9 - Quit")
         choice = int(input("Enter your choice: "))
         if choice == 9:
             keep_running = False
+        elif choice == 1:
+            HDL_Driver()
 
     print(choice)
     return(choice)
 
+def HDL_Driver():
+    HDL_value = hdl_input()
+    HDL_Character = hdl_analysis(HDL_Value)
+    hdl_output(HDL_Character)
+
+def hdl_input():
+    hdl_value = int(input("Enter HDL Value: "))
+    return hdl_value
+
+def hdl_analysis(HDL_value):
+    if HDL_value >= 60:
+        return "Normal"
+    elif 40 <= HDL_value <= 60:
+        return "Borderline Low"
+    else:
+        return "Low"
+
+def hdl_output(HDL_value, HDL_answer):
+    print("The HDL value of {} is considered {}".format(HDL_value, HDL_answer))
+    return
 interface()
