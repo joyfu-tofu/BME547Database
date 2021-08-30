efe87505d0e6bf11a1e8590c1482f9fe5a49cc8f
Joy Fu <lingyi.fu@duke.edu>
2021-08-30 12:51:18 -0400

    modified interface so it will run until 9 is chosen

--- blood_calculator.py
+++ blood_calculator.py
@@ -1,10 +1,14 @@
 def interface():
-    print("My Program")
-    print("Options:")
-    print("9 - Quit")
-    choice = input("Enter your choice: ")
+    print("Blood Calculator")
+    keep_running = True
+    while keep_running:
+        print("Make a choice")
+        print("9 - Quit")
+        choice = int(input("Enter your choice: "))
+        if choice == 9:
+            keep_running = False
+
     print(choice)
     return(choice)
-
 
 interface()
