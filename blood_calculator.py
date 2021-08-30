52c5b51a472dd0e462ce4a08aa013573090c143a
Joy Fu <lingyi.fu@duke.edu>
2021-08-30 12:43:51 -0400

    make initial interface function

---
+++ blood_calculator.py
@@ -0,0 +1,10 @@
+def interface():
+    print("My Program")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    print(choice)
+    return(choice)
+
+
+interface()
